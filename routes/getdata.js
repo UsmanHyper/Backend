179a505ff5a1241c04c0fe83ec3bcb08b2aa24cb
UsmanHyper <usmanbinsaad01@gmail.com>
2024-01-11 21:24:33 +0500

    changes in get data

--- routes/getdata.js
+++ routes/getdata.js
@@ -8,7 +8,7 @@ router.get("/", authenticateToken, async (req, res) => {
 
   try {
     const page = Number(req.query.page) || 1;
-    const limit =Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit
     const sort = req.query.sortby || "name"
     const order = req.query.orderby || "asc"
